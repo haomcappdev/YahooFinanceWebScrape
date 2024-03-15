014cdc796f1ed397ef393de958b47e8b9aedf639
Hao <Hao@DESKTOP-LR6O3M3>
2024-03-15 23:02:10 +0800

    Windows filepath to stock code text file

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ def log_stock_info(stock_info_input):
 
 
 stock_codes = []
-f = open("/Users/chaiminghao/Documents/Minghao Learning/Test files/Python/YahooFinanceWebScrape/stock codes.txt", "r")
+#f = open("/Users/chaiminghao/Documents/Minghao Learning/Test files/Python/YahooFinanceWebScrape/stock codes.txt", "r")
+f = open('C:\\Users\\Hao\\Documents\\Learning\\Python\\YahooFinanceWebScrape\\stock codes.txt', "r")
 for line in f:
     stock_codes.append(line)
 f.close()
